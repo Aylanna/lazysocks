f675a287d8e7abd53005dc8170fd5e94f41bb53b
Nick Oosterhuis <nickoosterhuis@gmail.com>
2016-11-30 13:13:43 +0100

    Added whack a mole minigame

---
+++ lazysocks/Assets/Scripts/WhackAMoleScripts/Hole.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SocialPlatforms.Impl;
+
+public class Hole : MonoBehaviour
+{
+    private GameObject mole;
+    private int scoreValue = 10;
+
+    // Spawn Interval
+    private int intervalMin = 5;
+    private int intervalMax = 15;
+
+    // Use this for initialization
+    void Start () {
+        Invoke("Spawn", Random.Range(intervalMin, intervalMax));
+    }
+
+    //Spawn a mole game object in a certain interval
+    void Spawn() {
+
+        GameObject g = (GameObject)Instantiate(mole, transform.position, transform.rotation);
+        //g.GetComponent<Mole>(). = Random.Range(10,200);
+
+
+
+
+
+
+        Invoke("Spawn", Random.Range(intervalMin, intervalMax));
+
+
+    }
+}
---
+++ lazysocks/Assets/Scripts/WhackAMoleScripts/Mole.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mole : MonoBehaviour
+{
+    void OnMouseDown() {
+        Destroy(this);
+    }
+}
